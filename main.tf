152a27128fbb7ad6292051d0fcdfe3ed2f9dfca0
ksskandasaigiri <88838826+ksskandasaigiri@users.noreply.github.com>
2021-12-16 16:47:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name = "test-bucket-random-019122"
+  name = "test-bucket-random-019124"
   location = "EU"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-019123"
+  name = "test-bucket-random-019125"
   location = "EU"
 }
